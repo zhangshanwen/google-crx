014c53fb42b746a28d4634fdac537e2dfa61b9c8
shanwen.zhang <shanwen@atlasv.com>
2021-01-21 18:28:44 +0800

    push

--- src/aso/content_utils/frame.js
+++
@@ -1,9 +0,0 @@
-iframeElement = document.querySelector('iframe');
-// iframeElement.src = 'https://storage.googleapis.com/aso-tool-box-staging/frame-level2.html' + location.search + '&v=1169    ';
-iframeElement.src = 'https://storage.googleapis.com/aso-tool-box/frame-level2.html' + location.search + '&v=1169';
-
-const receiveMessage = (event) => {
-	window.parent.postMessage( event.data, '*');
-};
-
-window.addEventListener('message', receiveMessage, false);
--- src/aso/content_utils/inneriOS.js
+++
@@ -1,7 +0,0 @@
-if (!document.body.classList.contains('ASOToolOn')) {
-  document.addEventListener('click', (event) => {
-    if (event.target.href) {
-      window.top.document.location.href = event.target.href;
-    }
-  }, false);
-}
